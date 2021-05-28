3b42c36bb6c8cf3355d8b070ecfb7a75426d6d8e
Daniel Everland <danieleverland@gmail.com>
2021-05-28 22:33:16 +0200

    Added raw calculations for two samples

---
+++ R/AnalysisOfTwoSamples.R
@@ -0,0 +1,49 @@
+#' @title Raw tobs of two samples
+#' @description Calculates the tobs for two samples using raw data
+#' @param m Vector of 2 means
+#' @param v Vector of 2 variances
+#' @param n Vector of 2 sample sizes
+#' @return The t-obs value
+#' @export
+tobs2Raw = function(m, v, n) {
+  (m[1]-m[2])/sqrt(v[1]/n[1]+v[2]/n[2])
+}
+
+#' @title Raw dof of two samples
+#' @description Calculates the degrees of freedom for two samples using raw data
+#' @param v Vector of 2 variances
+#' @param n Vector of 2 sample sizes
+#' @return The degrees of freedom
+#' @export
+dof2Raw = function(v, n) {
+  ((v[1]/n[1]+v[2]/n[2])^2)/((v[1]/n[1])^2/(n[1]-1)+(v[2]/n[2])^2/(n[2]-1))
+}
+
+#' @title Raw two sample p-value
+#' @description Calculates the p-val for two samples using raw data
+#' @param m Vector of 2 means
+#' @param v Vector of 2 variances
+#' @param n Vector of 2 sample sizes
+#' @return The t-test p-value
+#' @export
+pval2Raw = function(m, v, n) {
+  2 * (1- pt(tobs2Raw(m, v, n), dof2Raw(v, n)))
+}
+
+#' @title Two sample confidence interval for mean1 - mean2
+#' @description Calculates the confidence interval for two samples using raw data
+#' @param m Vector of 2 means
+#' @param v Vector of 2 variances
+#' @param n Vector of 2 sample sizes
+#' @param alpha Significance level (0.05 by default)
+#' @return Confidence interval for the difference in mean of x and y with significance level alpha
+#' @export
+confInterval2Raw = function(m, v, n, alpha = 0.05) {
+  xbar = m[1]
+  ybar = m[2]
+  s1 = v[1]
+  s2 = v[2]
+  n1 = n[1]
+  n2 = n[2]
+  xbar - ybar+c(-1,1)*qt(1-alpha/2, df = dof2Raw(v, n))*sqrt((s1/n1)+(s2/n2))
+}
